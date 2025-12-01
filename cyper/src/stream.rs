444220d917c9989c89712d47d892c96c20b61a81
王宇逸 <Strawberry_Str@hotmail.com>
2025-12-01 17:12:44 +0800

    feat(cyper): use MaybeTlsStream

--- cyper/src/stream.rs
+++ cyper/src/stream.rs
@@ -1,125 +1,42 @@
 use std::{
-    borrow::Cow,
     io,
     pin::Pin,
     task::{Context, Poll},
 };
 
-#[cfg(any(feature = "native-tls", feature = "rustls"))]
-use compio::tls::TlsStream;
-use compio::{
-    buf::{BufResult, IoBuf, IoBufMut, IoVectoredBuf, IoVectoredBufMut},
-    io::{AsyncRead, AsyncWrite},
-    net::TcpStream,
-};
+use compio::{buf::IoBuf, net::TcpStream, tls::MaybeTlsStream};
 use cyper_core::HyperStream;
 use hyper::Uri;
 use hyper_util::client::legacy::connect::{Connected, Connection};
 
 use crate::{Error, Result, TlsBackend};
 
-#[allow(clippy::large_enum_variant)]
-enum HttpStreamInner {
-    Tcp(TcpStream),
-    #[cfg(any(feature = "native-tls", feature = "rustls"))]
-    Tls(TlsStream<TcpStream>),
-}
+/// A HTTP stream wrapper, based on compio, and exposes [`hyper::rt`]
+/// interfaces.
+pub struct HttpStream(HyperStream<MaybeTlsStream<TcpStream>>);
 
-impl HttpStreamInner {
+impl HttpStream {
+    /// Create [`HttpStream`] with target uri and TLS backend.
     pub async fn connect(uri: Uri, tls: TlsBackend) -> Result<Self> {
         let scheme = uri.scheme_str().unwrap_or("http");
         let host = uri.host().expect("there should be host");
         let port = uri.port_u16();
-        match scheme {
+        let stream = match scheme {
             "http" => {
                 let stream = TcpStream::connect((host, port.unwrap_or(80))).await?;
                 // Ignore it.
                 let _tls = tls;
-                Ok(Self::Tcp(stream))
+                MaybeTlsStream::new_plain(stream)
             }
             #[cfg(any(feature = "native-tls", feature = "rustls"))]
             "https" => {
                 let stream = TcpStream::connect((host, port.unwrap_or(443))).await?;
                 let connector = tls.create_connector()?;
-                Ok(Self::Tls(connector.connect(host, stream).await?))
+                MaybeTlsStream::new_tls(connector.connect(host, stream).await?)
             }
-            _ => Err(Error::BadScheme(scheme.to_string())),
-        }
-    }
-
-    fn negotiated_alpn(&self) -> Option<Cow<'_, [u8]>> {
-        match self {
-            Self::Tcp(_) => None,
-            #[cfg(any(feature = "native-tls", feature = "rustls"))]
-            Self::Tls(s) => s.negotiated_alpn(),
-        }
-    }
-}
-
-impl AsyncRead for HttpStreamInner {
-    async fn read<B: IoBufMut>(&mut self, buf: B) -> BufResult<usize, B> {
-        match self {
-            Self::Tcp(s) => s.read(buf).await,
-            #[cfg(any(feature = "native-tls", feature = "rustls"))]
-            Self::Tls(s) => s.read(buf).await,
-        }
-    }
-
-    async fn read_vectored<V: IoVectoredBufMut>(&mut self, buf: V) -> BufResult<usize, V> {
-        match self {
-            Self::Tcp(s) => s.read_vectored(buf).await,
-            #[cfg(any(feature = "native-tls", feature = "rustls"))]
-            Self::Tls(s) => s.read_vectored(buf).await,
-        }
-    }
-}
-
-impl AsyncWrite for HttpStreamInner {
-    async fn write<T: IoBuf>(&mut self, buf: T) -> BufResult<usize, T> {
-        match self {
-            Self::Tcp(s) => s.write(buf).await,
-            #[cfg(any(feature = "native-tls", feature = "rustls"))]
-            Self::Tls(s) => s.write(buf).await,
-        }
-    }
-
-    async fn write_vectored<T: IoVectoredBuf>(&mut self, buf: T) -> BufResult<usize, T> {
-        match self {
-            Self::Tcp(s) => s.write_vectored(buf).await,
-            #[cfg(any(feature = "native-tls", feature = "rustls"))]
-            Self::Tls(s) => s.write_vectored(buf).await,
-        }
-    }
-
-    async fn flush(&mut self) -> io::Result<()> {
-        match self {
-            Self::Tcp(s) => s.flush().await,
-            #[cfg(any(feature = "native-tls", feature = "rustls"))]
-            Self::Tls(s) => s.flush().await,
-        }
-    }
-
-    async fn shutdown(&mut self) -> io::Result<()> {
-        match self {
-            Self::Tcp(s) => s.shutdown().await,
-            #[cfg(any(feature = "native-tls", feature = "rustls"))]
-            Self::Tls(s) => s.shutdown().await,
-        }
-    }
-}
-
-/// A HTTP stream wrapper, based on compio, and exposes [`hyper::rt`]
-/// interfaces.
-pub struct HttpStream(HyperStream<HttpStreamInner>);
-
-impl HttpStream {
-    /// Create [`HttpStream`] with target uri and TLS backend.
-    pub async fn connect(uri: Uri, tls: TlsBackend) -> Result<Self> {
-        Ok(Self::from_inner(HttpStreamInner::connect(uri, tls).await?))
-    }
-
-    fn from_inner(s: HttpStreamInner) -> Self {
-        Self(HyperStream::new(s))
+            _ => return Err(Error::BadScheme(scheme.to_string())),
+        };
+        Ok(Self(HyperStream::new(stream)))
     }
 }
 
