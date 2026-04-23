f99c3c2b59735e952d87f4d377166a31b4b19cc1
Yuyi Wang <Strawberry_Str@hotmail.com>
2026-04-23 20:23:12 +0800

    feat(core)!: remove `CompioStream`

--- cyper-core/src/stream.rs
+++ cyper-core/src/stream.rs
@@ -7,7 +7,6 @@ use std::{
 };
 
 use compio::{
-    buf::{BufResult, IoBuf, IoBufMut},
     io::{AsyncRead, AsyncWrite, util::Splittable},
     tls::{MaybeTlsStream, TlsStream},
 };
@@ -98,79 +97,5 @@ where
 
     fn poll_shutdown(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
         futures_util::AsyncWrite::poll_close(Pin::new(&mut *self.0), cx)
-    }
-}
-
-/// A wrapper that bridges hyper's poll-based IO (`hyper::rt::Read + Write`)
-/// to compio's ownership-based async IO (`AsyncRead + AsyncWrite`).
-///
-/// This is the reverse of [`HyperStream`]: where `HyperStream` wraps a compio
-/// stream for use with hyper, `CompioStream` wraps a hyper stream for use with
-/// compio.
-pub struct CompioStream<S>(S);
-
-impl<S> CompioStream<S> {
-    /// Create a compio stream wrapper from a hyper-compatible stream.
-    pub fn new(s: S) -> Self {
-        Self(s)
-    }
-
-    /// Get a reference to the inner stream.
-    pub fn get_ref(&self) -> &S {
-        &self.0
-    }
-
-    /// Get a mutable reference to the inner stream.
-    pub fn get_mut(&mut self) -> &mut S {
-        &mut self.0
-    }
-
-    /// Consume the wrapper, returning the inner stream.
-    pub fn into_inner(self) -> S {
-        self.0
-    }
-}
-
-impl<S: std::fmt::Debug> std::fmt::Debug for CompioStream<S> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("CompioStream")
-            .field("inner", &self.0)
-            .finish()
-    }
-}
-
-impl<S: hyper::rt::Read + Unpin> AsyncRead for CompioStream<S> {
-    async fn read<B: IoBufMut>(&mut self, mut buf: B) -> BufResult<usize, B> {
-        let result = std::future::poll_fn(|cx| {
-            let uninit: &mut [MaybeUninit<u8>] = buf.as_uninit();
-            let mut read_buf = hyper::rt::ReadBuf::uninit(uninit);
-            ready!(Pin::new(&mut self.0).poll_read(cx, read_buf.unfilled()))?;
-            Poll::Ready(Ok::<_, io::Error>(read_buf.filled().len()))
-        })
-        .await;
-        match result {
-            Ok(n) => {
-                // SAFETY: poll_read initialized `n` bytes from the beginning.
-                unsafe { buf.set_len(n) };
-                BufResult(Ok(n), buf)
-            }
-            Err(e) => BufResult(Err(e), buf),
-        }
-    }
-}
-
-impl<S: hyper::rt::Write + Unpin> AsyncWrite for CompioStream<S> {
-    async fn write<T: IoBuf>(&mut self, buf: T) -> BufResult<usize, T> {
-        let result =
-            std::future::poll_fn(|cx| Pin::new(&mut self.0).poll_write(cx, buf.as_init())).await;
-        BufResult(result, buf)
-    }
-
-    async fn flush(&mut self) -> io::Result<()> {
-        std::future::poll_fn(|cx| Pin::new(&mut self.0).poll_flush(cx)).await
-    }
-
-    async fn shutdown(&mut self) -> io::Result<()> {
-        std::future::poll_fn(|cx| Pin::new(&mut self.0).poll_shutdown(cx)).await
     }
 }
